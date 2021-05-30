4eb9aa2d6582ce9d42d3b52941cc2bc5bf4d60bf
Alan Goldberg <alan.goldberg@yahoo.com>
2021-05-30 15:31:50 -0400

    D3: Add an additional map

--- static/js/challenge_logic.js
+++ static/js/challenge_logic.js
@@ -1,7 +1,20 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 
-// We create the tile layer that will be the background of our map.
+// We create the tile layers that will be the background of our map.
+/*
+    Alternative map styles
+    mapbox/streets-v11
+    mapbox/outdoors-v11
+    mapbox/light-v10
+    mapbox/dark-v10
+    mapbox/satellite-v9
+    mapbox/satellite-streets-v11
+    mapbox/navigation-day-v1
+    mapbox/navigation-night-v1
+*/
+
+// Create the tile layer that will be the background of our map.
 let streets = L.tileLayer(
   "https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}",
   {
@@ -12,9 +25,20 @@ let streets = L.tileLayer(
   }
 );
 
-// We create the second tile layer that will be the background of our map.
+// Create the second tile layer that will be the background of our map.
 let satelliteStreets = L.tileLayer(
   "https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}",
+  {
+    attribution:
+      'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY,
+  }
+);
+
+// Create the third tile layer that will be the background of our map.
+let navigationNight = L.tileLayer(
+  "https://api.mapbox.com/styles/v1/mapbox/navigation-night-v1/tiles/{z}/{x}/{y}?access_token={accessToken}",
   {
     attribution:
       'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -34,6 +58,7 @@ let map = L.map("mapid", {
 let baseMaps = {
   Streets: streets,
   Satellite: satelliteStreets,
+  NightNavigation: navigationNight,
 };
 
 // 1. Add a 2nd layer group for the tectonic plate data.
